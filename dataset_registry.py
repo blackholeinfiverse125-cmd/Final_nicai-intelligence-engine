6fc994eb527ca79af8df3227aacfed3fd2e724ac
blackholeinfiverse116-ship-it <blackholeinfiverse116@gmail.com>
2026-04-16 17:41:58 +0530

    Update dataset_registry.py

--- dataset_registry.py
+++ dataset_registry.py
@@ -1,18 +1,16 @@
 import json
 
+
 def load_datasets():
     try:
         with open("datasets.json", "r") as f:
-            return json.load(f)
+            return json.load(f) ा
     except FileNotFoundError:
         print("ERROR: datasets.json file not found")
-        return []
+        return {}
+
 
 def get_dataset(dataset_id):
     datasets = load_datasets()
 
-    for dataset in datasets:
-        if dataset["dataset_id"] == dataset_id:
-            return dataset
-
-    return None
+    return datasets.get(dataset_id)   # ✅ DIRECT ACCESS
